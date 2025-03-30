a58317c583524dfa648964f4104cca0870fc01de
AnandhiRuckmoni <babasai.sr@gmail.com>
2025-03-30 11:14:17 +0530

    Time: 11 ms (29.52%) | Memory: 19.2 MB (87.32%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,18 +1,11 @@
-import sys
+
+
 class Solution:
     def majorityElement(self, nums: list) -> int:
         d={}
-        m=-sys.maxsize
-        n=0
         for i in nums:
             if i not in d.keys():
                 d[i]=1
             else:
                 d[i]+=1
-        
-        for i,j in d.items():
-            if j > m:
-                m=j
-                n=i
-        
-        return n
+        return (max(d,key=d.get))
