61891d5a79e70efe7f3cd399d9953627f042f7c3
AnandhiRuckmoni <babasai.sr@gmail.com>
2025-06-16 01:01:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (14.95%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -5,29 +5,14 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    def getSubtree(self,root):
-        l=[]
-        curr=root
-        if curr.val not in l:
-            l.append(curr.val)
-        if curr.left:
-            l+=self.getSubtree(curr.left)
-        if curr.right:
-            l+=self.getSubtree(curr.right)
-        return l
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         l=[]
         if not self:
-            return None
-        
+            return None        
         if val==root.val:
-            print(val)
-            #l=self.getSubtree(root)
-            return root
-            print(l)
+            return root            
         elif val < root.val:
             if root.left:
-                print(root.val,'root.left')
                 return self.searchBST(root.left,val)
         elif val > root.val:
             if root.right:
