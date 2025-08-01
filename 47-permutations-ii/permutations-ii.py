f8cfcaba661ee569764f072b262a5edc71b4a8ec
AnandhiRuckmoni <babasai.sr@gmail.com>
2025-08-01 12:09:18 +0530

    Time: 719 ms (5.02%) | Memory: 18 MB (78.52%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,23 +1,18 @@
 class Solution:
-    def permutation(self,l,ans,l1):
-        
+    def permutation(self,l,ans,l1):        
         l2=[]
         if len(l1)==len(l):
             l2=[l[i] for i in l1]
             if l2 not in ans:
                 ans.append(l2)
             return
-        for i in range(len(l)):
+        for i in range(len(l)):            
             if i not in l1:
                 l1.append(i)
-                self.permutation(l,ans,l1)
-                
+                self.permutation(l,ans,l1)                              
                 l1.pop()
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         ans=[]
         l1=[]
-        
-        
         self.permutation(nums,ans,l1)
         return ans
-        
