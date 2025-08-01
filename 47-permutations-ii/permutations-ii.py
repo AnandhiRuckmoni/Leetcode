cd26840cca94bf05d18f842b4f01b3cd2d88017e
AnandhiRuckmoni <babasai.sr@gmail.com>
2025-08-01 11:51:56 +0530

    Time: 725 ms (5.02%) | Memory: 18.2 MB (35.31%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,13 +1,12 @@
 class Solution:
     def permutation(self,l,ans,l1):
+        
         l2=[]
         if len(l1)==len(l):
             l2=[l[i] for i in l1]
             if l2 not in ans:
                 ans.append(l2)
             return
-        
-        
         for i in range(len(l)):
             if i not in l1:
                 l1.append(i)
@@ -17,6 +16,8 @@ class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         ans=[]
         l1=[]
+        
+        
         self.permutation(nums,ans,l1)
         return ans
         
