0e204f667d935c6fe949e57b45288b5c37106361
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-01-25 20:19:05 +0530

    Time: 231 ms (5.99%) | Memory: 21.9 MB (6.78%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,10 +1,42 @@
+import sys
 class Solution:
-        def removeDuplicates(self, nums: list) -> int:
-            d={}
-            for i in nums:
-                d[i]=1
-            nums.clear()
-            for i in d.keys():
-                nums.append(i)
-            return len(nums)       
-                    
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i=0
+        j=len(nums)-1
+        dups=[]
+        unique=[]
+        x=-sys.maxsize
+        while i < j:
+            if nums[i] !=nums[j]:
+                if nums[i] not in unique:
+                    unique.append(nums[i])
+                else:
+                    dups.append(i)
+                if nums[j] not in unique:
+                    unique.append(nums[j])
+                else:
+                    dups.append(j)
+            else:
+                if nums[i] not in unique:
+                    unique.append(nums[i])
+                    dups.append(j)
+                else:
+                    dups.append(i)
+                    dups.append(j)
+            i+=1
+            j-=1
+        if i ==j:
+            if nums[i] not in unique:
+                unique.append(nums[i])
+            else:
+                dups.append(i)
+        print(i,j,dups,unique)
+        for i in dups:
+            nums.append(nums[i])
+            nums[i] = x
+        i=0
+        for i in nums[:]:
+            if i==x:
+                nums.remove(i)
+        
+        return len(unique)
