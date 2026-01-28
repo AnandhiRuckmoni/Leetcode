4c569e7d88db09f38bc90c19999ae91c466807b3
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-01-28 20:35:03 +0530

    Time: 35 ms (16.36%) | Memory: 19.5 MB (12.96%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -2,43 +2,38 @@ import sys
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
                 
-        s1=""
+        start = 0
+        cnt=0
         l=[]
-        l1=[]
-        repeating=[]
-        c=0
-        maxval=-sys.maxsize
-        if len(s) == 0 : 
+        if s=="":
             return 0
-        for i in range(len(s)):
-            
-            l.append(s[i])
-            s1=s[i]    
-            
-            for j in range(i+1,len(s)):
-                
-                s1+=s[j]
-                if len(s1) > maxval:
-                    if s1 not in repeating:
-                        if len([letter for letter in s1 if s1.count(letter) > 1]) > 0 :
-                            repeating.append(s1)
-                            break
-                        if s1 not in l:
-                            #print(s1,l[len(l)-1])
-                            if len(s1) > len(l[len(l)-1]):
-                                
-                                l.append(s1)
-                    else:
-                        break
-
-            if len(l) > 1000 :        
-                c=len(l[len(l)-1])
-                if c > maxval : 
-                    maxval=c
-                l.clear()
-            
+        l.append(s[start])
+        start +=1
+        
+        while start <len(s):
+            if l!=[]: 
+                if s[start] not in l:
+                    l.append(s[start])
+                else:
+                    
+                    indx=l.index(s[start])
+                    
+                    if cnt < len(l):
+                        cnt=len(l)
+                        
+                    
+                    val=start-len(l)+indx+1
+                    #print(l,start,indx,val)
+                    l.clear()
+                    l=list(s[val:start])
+                    
+                    l.append(s[start])
+                    #print(l)
+                    
             else:
-                c=max([len(i) for i in l])
-                if c > maxval : 
-                    maxval=c
-        return maxval
+                l.append(s[start])
+            start +=1
+        if l!=[]:
+            if cnt < len(l):
+                cnt=len(l)
+        return cnt
