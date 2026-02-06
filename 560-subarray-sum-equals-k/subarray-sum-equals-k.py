64cb6e5454f4facff81d900eeb6ca63f6b90a311
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-02-06 12:58:26 +0530

    Time: 47 ms (11.47%) | Memory: 22 MB (37.18%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,6 +1,6 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        d={}
+        """d={}
         d[0]=1
         currsum = 0
         cnt=0
@@ -12,4 +12,22 @@ class Solution:
                 d[currsum]+=1
             else:
                 d[currsum]=1
+        return cnt
+"""
+        i=0
+        j=0
+        d={}
+        d[0]=1
+        summ=0
+        cnt=0
+        
+        while j < len(nums):
+            summ+=nums[j]
+            if summ-k in d.keys():
+                cnt+=d[summ-k]
+            if summ in d.keys():
+                d[summ]+=1
+            else:
+                d[summ]=1
+            j+=1
         return cnt
