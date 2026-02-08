6cb0e61f8ae5fe0cd74c4e40715b064af9d3e2c8
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-02-08 09:46:21 +0530

    Time: 211 ms (21.55%) | Memory: 20.1 MB (10.19%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,6 +1,7 @@
+from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        i=0
+        """i=0
         j=0
         l=[]
         s1=""
@@ -17,4 +18,34 @@ class Solution:
                 s1=s1[1:]
                 i+=1
                 j+=1
-        return l
+        return l"""
+        t1=Counter(p)
+        t2=Counter()
+        print(t1,t2)
+        j=0
+        i=0
+        l=[]
+        l1=[]
+        cnt=0
+        while j < len(s):
+            cnt+=1
+
+            if s[j] in t2:
+                t2[s[j]]+=1
+            else:
+                t2[s[j]]=1
+            if cnt<len(p):
+                j+=1
+            else:
+                
+                if t1==t2:
+                    l1.append(i)
+                #print(t2,i,j,l1)
+                if s[i] in t2.keys():
+                    t2[s[i]]-=1
+                    if t2[s[i]]==0:
+                        del t2[s[i]]
+                i+=1
+                j+=1
+                #print(i,j,t2)
+        return l1
