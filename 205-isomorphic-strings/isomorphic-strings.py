72e70ce1c74cf1cb1e2c46432bcb91fe01e965d2
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-03-03 23:33:47 +0530

    Time: 5 ms (50.68%) | Memory: 19.2 MB (73.88%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        d={}
+        for i in range(len(s)):
+            if s[i] not in d.keys():
+                if t[i] not in d.values():
+                    d[s[i]]=t[i]
+                else:
+                    return False
+            else:
+                if d[s[i]]!=t[i]:
+                    return False
+        return True
