62362a373bb613682618a3db251a316a4923065a
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-03-29 18:37:40 +0530

    Time: 3 ms (44.36%) | Memory: 20.6 MB (30.54%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,42 +1,12 @@
-import sys
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         i=0
-        j=len(nums)-1
-        dups=[]
-        unique=[]
-        x=-sys.maxsize
-        while i < j:
-            if nums[i] !=nums[j]:
-                if nums[i] not in unique:
-                    unique.append(nums[i])
-                else:
-                    dups.append(i)
-                if nums[j] not in unique:
-                    unique.append(nums[j])
-                else:
-                    dups.append(j)
+        j=i+1
+        while i <=j and j < len(nums):
+            if nums[i]==nums[j]:
+                j+=1
             else:
-                if nums[i] not in unique:
-                    unique.append(nums[i])
-                    dups.append(j)
-                else:
-                    dups.append(i)
-                    dups.append(j)
-            i+=1
-            j-=1
-        if i ==j:
-            if nums[i] not in unique:
-                unique.append(nums[i])
-            else:
-                dups.append(i)
-        print(i,j,dups,unique)
-        for i in dups:
-            nums.append(nums[i])
-            nums[i] = x
-        i=0
-        for i in nums[:]:
-            if i==x:
-                nums.remove(i)
-        
-        return len(unique)
+                nums[i+1],nums[j]=nums[j],nums[i+1]
+                i+=1
+                j+=1
+        return i+1
