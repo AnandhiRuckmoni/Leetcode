d25d871f50fa95d4773aba001fb3d4a2902abb74
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-03-30 17:50:20 +0530

    Time: 22 ms (6.19%) | Memory: 21.3 MB (21.25%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,14 +1,17 @@
 import math
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
+        
+        
+
         #BruteForce
         #return sorted([i*i for i in nums])
         # TC = o(nlogn) 
 
-
+#-------------------------------------------------------------------------
         #optimized
 
-        i=0
+        """   i=0
         j=len(nums)-1
         l=[]
         while i <=j:
@@ -18,4 +21,30 @@ class Solution:
             else:
                 l=[nums[i] * nums[i]] +l
                 i+=1
+        return l"""
+
+#-------------------------------------------------------------------------
+        pos=[i for i in nums if i>=0]
+        neg=[i for i in nums if i < 0]
+        pos=[i*i for i in pos]
+        neg=[i*i for i in neg]
+        neg=neg[::-1]
+        i=0
+        j=0
+        
+        l=[]
+        print(pos,neg)
+        while i < len(neg) and j < len(pos):
+            if neg[i] <= pos[j]:
+                l.append(neg[i])
+                i+=1
+            else:
+                l.append(pos[j])
+                j+=1
+        while i < len(neg):
+            l.append(neg[i])
+            i+=1
+        while j < len(pos):
+            l.append(pos[j])
+            j+=1
         return l
