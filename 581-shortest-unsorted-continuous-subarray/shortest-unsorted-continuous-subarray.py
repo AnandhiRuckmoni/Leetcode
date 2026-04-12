291186b6d43a926cffc6188082792ca5cbcb21ff
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-04-12 20:10:42 +0530

    Time: 109 ms (6.65%) | Memory: 21 MB (7.19%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -1,8 +1,10 @@
+import sys
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
         if nums==sorted(nums) or len(nums)==1:
             return 0
-        l=sorted(nums)
+        #Copy the array to another array, sort and compare the indices for missorted order - o(nlogn)    
+        """l=sorted(nums)
         i=0
         stidx=-1
         edidx=-1
@@ -20,5 +22,25 @@ class Solution:
                     edidx=i
                     break
             i-=1
-        return edidx-stidx+1
-        
+        return edidx-stidx+1"""
+        i=0
+        j=len(nums)-1
+        mx=-sys.maxsize
+        mn=sys.maxsize
+        stidx=-1
+        edidx=-1
+        while i < len(nums) or j >=0:
+            if i < len(nums):
+                mx=max(mx,nums[i])
+                if nums[i] < mx:
+                    print(i,mx)
+                    stidx=i
+                i+=1
+            if j >=0:
+                mn=min(mn,nums[j])
+                if nums[j]>mn:
+                    print(j,mn)
+                    edidx=j
+                j-=1
+        print(stidx,edidx)
+        return stidx-edidx+1
