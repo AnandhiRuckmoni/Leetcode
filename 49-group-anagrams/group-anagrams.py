b5667d9ee4ed52bd7157286a15f5c1e5945e0eb9
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-04-16 17:40:54 +0530

    Time: 23 ms (10.90%) | Memory: 22.7 MB (41.19%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,33 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d={}
-
+        
         l=[]
+        d={}
+        d1={}
         l1=[]
         for i in strs:
-            val="".join(sorted(i))            
-            if val not in d.keys():
-                l.append(i)
-                d[val]=l
-                l=[]
+            if len(d)==0:
+                d[i]=[]
+                d1["".join(sorted(i))]=i
             else:
-                d[val].append(i)
-        for i in d.values():
-            l1.append(i)
+                x="".join(sorted(i))
+                #found=False
+                #for j in d.keys():
+                if x in d1.keys():
+                    #y="".join(sorted(j))
+                    #if x==y:
+                    d[d1[x]].append(i)
+                    #found=True
+                    #break
+                else:
+                    d[i]=[]
+                    d1["".join(sorted(i))]=i
+            #print(l1)
+        for i,j in d.items():
+            l.append(i)
+            if d[i] !=[]:
+                l=l+d[i]
+            l1.append(l)
+            l=[]
+        #print(l1)
         return l1
