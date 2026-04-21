19e377a55a34e97279f0657af325a22921270630
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-04-21 10:36:17 +0530

    Time: 13 ms (50.43%) | Memory: 19.2 MB (73.11%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,39 +1,21 @@
-import sys
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-                
-        start = 0
-        cnt=0
-        l=[]
-        if s=="":
-            return 0
-        l.append(s[start])
-        start +=1
-        
-        while start <len(s):
-            if l!=[]: 
-                if s[start] not in l:
-                    l.append(s[start])
-                else:
-                    
-                    indx=l.index(s[start])
-                    
-                    if cnt < len(l):
-                        cnt=len(l)
-                        
-                    
-                    val=start-len(l)+indx+1
-                    #print(l,start,indx,val)
-                    l.clear()
-                    l=list(s[val:start])
-                    
-                    l.append(s[start])
-                    #print(l)
-                    
+        if len(s)==1: return 1
+        i=0
+        j=0
+        s1=""
+        mx=0
+        while j < len(s):
+            if s[j] not in s1:
+                s1+=s[j]
             else:
-                l.append(s[start])
-            start +=1
-        if l!=[]:
-            if cnt < len(l):
-                cnt=len(l)
-        return cnt
+                while s[j] in s1:
+                    mx=max(mx,len(s1))
+                    s2=s1[1:]
+                    s1=s2
+                    i+=1
+                s1+=s[j]
+            j+=1
+            #print(s1,mx,i,j)
+        mx=max(mx,len(s1))
+        return mx
