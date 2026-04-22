f4ffc507410cbf38bd0d047942b66481ef1c33ca
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-04-22 15:45:25 +0530

    Time: 67 ms (64.17%) | Memory: 20 MB (5.84%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,34 +1,26 @@
 from collections import Counter
+import sys
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if s==t:
-            return s
-        if len(t) > len(s):
-            return ""
-        l=Counter(t)
-        compare_len=len(t)
-        l1=[]
         i=0
         j=0
+        d=Counter(t)
+        clen=len(t)
         min_len=sys.maxsize
-        while j < len(s):            
-            if l[s[j]]>0:
-                compare_len-=1
-            l[s[j]]-=1
+        while j < len(s):
+            if d[s[j]]>0:
+                clen-=1
+            d[s[j]]-=1
             j+=1
-            while compare_len==0:
-                #l1.append(s[i:j])
-                #l=Counter(t)
+            while clen ==0:
                 if j-i < min_len:
                     min_len=j-i
                     start=i
-                l[s[i]]+=1
-                if l[s[i]] > 0:
-                    compare_len+=1
+                d[s[i]]+=1
+                if d[s[i]] > 0:
+                    clen+=1
                 i+=1
         if min_len==sys.maxsize:
             return ""
-        
-        
-        return(s[start:start+min_len])
-        
+        else:
+            return s[start:start+min_len]
