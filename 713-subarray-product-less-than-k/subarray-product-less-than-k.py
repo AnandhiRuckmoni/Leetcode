70432360c01799a80b1760b71457eb30abbd030c
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-06-06 10:04:07 +0530

    Time: 765 ms (5.04%) | Memory: 22.1 MB (81.95%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.py
@@ -1,44 +1,36 @@
 import math
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        
-        d={}
-        #l=[]
-        #l=list(set(nums))
+        n=len(nums)
+        if k==0:
+            return 0
         if math.prod(nums) < k:
-             return (len(nums)*(len(nums)+1))//2
-        """if len(l)==1 and math.prod(nums)<k:
-            x=len(nums)
-            return (x*(x+1))//2"""
-        #nums.sort()
-        
+            return (n*(n+1)//2)
         i=0
-        j=i+1
-        while i <len(nums):
-            #print(i,j)
-            if  nums[i] < k and (i) not in d.keys():
-                d[(i)]=1
-            
-            if  nums[j] < k and (j) not in d.keys():
-                d[(j)]=1
-            x=math.prod(nums[i:j+1])
-            t=(i) if i==j else tuple(range(i,j+1))
-            #print(i,j,x,tuple(range(i,j+1)),t)
-            
-            if x < k and t not in d.keys():
-                d[t]=1
+        j=i
+        cnt=0
+        p=math.prod(nums[i:j+1])
+        while i < n:            
+            #print(i,j,p,cnt)
+            if  p< k:
+                cnt+=1
                 j+=1
-                if j> len(nums)-1 :                
-                    i+=1
-                    if i >= len(nums)-1:
-                        j=i
-                    else:
-                        j=i+1
+                if j < n:
+                    p*=nums[j]
             else:
+                p//=nums[i]
                 i+=1
-                if i >= len(nums)-1:
-                    j=i
-                else:
-                    j=i+1
-        #print(nums,d.keys())
-        return(len(d))
+                temp=math.prod(nums[i+1:j+1])
+                p//=temp
+                j=i
+                if j < n:
+                    p=nums[i]
+                #print(i,j,p,cnt)
+                
+            if i<n and j >=n:
+                p/=nums[i]
+                i+=1
+                temp=math.prod(nums[i+1:j+1])
+                p//=temp     
+                j=i
+        return(cnt)
