2096a636a01535b9107a5c701fcb1d4dda7afd44
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-06-12 23:42:14 +0530

    Time: 71 ms (20.35%) | Memory: 22.2 MB (23.07%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,39 +1,36 @@
-import sys
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
+        mx=-1        
+        i=0
+        j=0
         cnt=0
-        mxlen=-sys.maxsize
         if k==0:
             for i in nums:
                 if i ==1:
                     cnt+=1
                 else:
-                    mxlen=max(cnt,mxlen)
+                    mx=max(cnt,mx)
                     cnt=0
-                mxlen=max(cnt,mxlen)
-            return mxlen
-        else:
-            cnt=0
-            mxlen=-sys.maxsize
-            i=0
-            j=0
-            while j < len(nums):
-                print(i,j,cnt)
-                if nums[j]==1:
+            
+            mx=max(cnt,mx)
+            return(mx)
+        while i <=j and j < len(nums):
+            if nums[j]==1:
+                j+=1
+            else:
+                if cnt < k:
+                    cnt+=1
                     j+=1
                 else:
-                    if cnt <= k:
-                        cnt+=1                        
-                    if cnt > k:                        
-                        mxlen=max(j-i,mxlen)
-                        while cnt > k:                            
-                            if nums[i]==0:
-                                cnt-=1
-                            i+=1                        
-                    j+=1
-                print(mxlen)
-            mxlen=max(j-i,mxlen)
-            return(mxlen)
-
-
+                    mx=max(j-i,mx)
+                    #print(i,j,mx)
+                    while cnt==k:
+                        if nums[i]==1:
+                            i+=1
+                        else:
+                            cnt-=1
+                            i+=1
+                    
         
+        mx=max(j-i,mx)
+        return(mx)
