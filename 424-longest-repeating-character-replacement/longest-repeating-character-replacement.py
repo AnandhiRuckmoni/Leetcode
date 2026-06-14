48b36402fdeacf37b455ac6cd420905c3053ced3
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-06-14 16:20:00 +0530

    Time: 451 ms (5.01%) | Memory: 19.7 MB (15.52%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,55 +1,31 @@
-from collections import Counter
-import sys
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # How to flip anyting other than A to A at the most k times to get the longest consecutive As
-        cnt=0
-        mxlen=-sys.maxsize
-        l1=[]
-        if k==0:
-            for i in s:
-                print(i,l1)
-                if i not in l1 and l1==[]:
-                    l1.append(i)
-                elif i not in l1 and l1!=[]:
-                    mxlen=max(len(l1),mxlen)
-                    l1=[]
-                    l1.append(i)
-                elif i in l1:
-                    l1.append(i)
-                print(mxlen)
-            if  mxlen==-sys.maxsize or l1!=[]:
-                mxlen=max(len(l1),mxlen)
-            print(mxlen)
-            return(mxlen)
-        else:
-            cnt=0
-            mxlen=-sys.maxsize
-            i=0
-            j=0
-            d={}
-            while j < len(s):                
-                if s[j] in d.keys():
-                    d[s[j]]+=1
-                else:
-                    d[s[j]]=1
-                windowsize=j-i+1
-                max_freq = max(d.values())
-                noofreplacements=windowsize-max_freq
-                #print(i,j,d,noofreplacements)
-                if noofreplacements <= k:
-                    j+=1
-                else:
-                    mxlen=max(j-i,mxlen)
-                    while noofreplacements > k:
+        mx=-1
+        i=0
+        j=0
+        d={}
+        while i <= j and j < len(s):
+            windowsize=j-i+1
+            if s[j] not in d.keys():
+                d[s[j]]=1
+            else:
+                d[s[j]]+=1
+            maxoccurence=sorted(d.items(), key=lambda x:-x[1])[0][1]
+            if windowsize-maxoccurence<=k:
+                j+=1
+            else:
+                mx=max(mx,j-i)
+                while  windowsize-maxoccurence>k:
+                    if s[i] in d.keys():
                         d[s[i]]-=1
-                        if d[s[i]]==0:
-                            del d[s[i]]
-                        i+=1
-                        windowsize=j-i+1
-                        max_freq = max(d.values())
-                        noofreplacements=windowsize-max_freq
-                    j+=1
-            mxlen=max(j-i,mxlen)
-            #print(mxlen)
-            return(mxlen)
+                    if d[s[i]]==0:
+                        del d[s[i]]
+                    i+=1
+                    windowsize=j-i+1
+                                        
+                    maxoccurence=sorted(d.items(),key=lambda x:-x[1])[0][1]
+                    #print(s1,t,i,j,windowsize-maxoccurence)
+                j+=1
+        mx=max(mx,j-i)
+        print(mx)
+        return(mx)
