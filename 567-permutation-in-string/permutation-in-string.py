84d8fa9ceb3fd238f38973c95dde0e9e8a2f437a
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-06-15 11:07:23 +0530

    Time: 1052 ms (13.45%) | Memory: 19.4 MB (53.74%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,13 +2,15 @@ from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         d=Counter(s1)
+        searchLength=len(s1)
         i=0
-        j=i+len(s1)-1
-        while j < len(s2):
-            d1=Counter(s2[i:j+1])
+        j=i+searchLength
+        while i <=j and j <= len(s2):
+            word=s2[i:j]
+            d1=Counter(word)
             if d==d1:
-                return(True)
+                return True
             else:
                 i+=1
                 j+=1
-        return(False)
+        return False
