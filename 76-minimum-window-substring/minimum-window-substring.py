a2d7daae79d03dcd6439f3ccf03aaf13e4cc93d5
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-06-16 21:00:14 +0530

    Time: 910 ms (5.01%) | Memory: 19.8 MB (37.40%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,26 +1,68 @@
 from collections import Counter
-import sys
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        
+        s1=""
+        d=Counter(t)
+        
+
+        d1={}
         i=0
         j=0
-        d=Counter(t)
-        clen=len(t)
-        min_len=sys.maxsize
-        while j < len(s):
-            if d[s[j]]>0:
-                clen-=1
-            d[s[j]]-=1
-            j+=1
-            while clen ==0:
-                if j-i < min_len:
-                    min_len=j-i
-                    start=i
-                d[s[i]]+=1
-                if d[s[i]] > 0:
-                    clen+=1
+        while i <= j and j < len(s):
+            flag=False
+            #print(i,j,d1)
+            if s[j] in d.keys():
+                if s[j] in d1.keys():
+                    d1[s[j]]+=1
+                else:
+                    d1[s[j]]=1
+            j+=1    
+            
+            
+            #print(d,d1,i,j,flag)
+            if len(d)==len(d1):
+                for k,v in d.items():
+                    if k not in d1.keys():
+                        flag=False
+                        break
+                    if v <= d1[k]:             
+                        flag=True
+                    else:
+                        flag=False
+                        break
+            while flag:   
+                #print(flag,i,j,d,d1)  
+                #print(s1,i,j,s[i:j],min(s1,s[i:j],key=len))
+                if s1=="":
+                    s1=s[i:j]
+                else:
+                    s1=min(s1,(s[i:j]),key=len)
+                
+                if s[i] in d1.keys():
+                    d1[s[i]]-=1
+                    if d1[s[i]]==0:
+                        del d1[s[i]]
                 i+=1
-        if min_len==sys.maxsize:
-            return ""
-        else:
-            return s[start:start+min_len]
+                #print(d,d1,i,j,d.keys()==d1.keys(),l)
+                if len(d)==len(d1):               
+                    for k,v in d.items():
+                        #print(k,v,d1)
+                        if k not in d1.keys():
+                            flag=False
+                            break
+                        if v <= d1[k]:             
+                            flag=True
+                        else:
+                            flag=False
+                            break
+                else:
+                    flag=False
+                if not flag:
+                    #print("flag false")
+                    break
+                    
+            
+        return(s1)
