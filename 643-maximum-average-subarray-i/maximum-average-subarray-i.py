69ea4b0f07999df5e754b5f43d7e22248f5680f3
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-06-19 13:27:30 +0530

    Time: 87 ms (20.96%) | Memory: 29 MB (74.83%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,16 +1,20 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         i=0
-        j=0
-        summ=0
-        mx=-sys.maxsize
-        while j < len(nums):
-            summ+=nums[j]
-            if j-i+1<k:
-                j+=1
-            else:
-                mx=max(mx,summ)
-                summ-=nums[i]
-                i+=1
-                j+=1
-        return mx/k
+        j=i+k
+        av=0.0
+        s=sum(nums[i:j])
+        av=s/k
+        mx=av
+        while i <=j and j < len(nums):
+            s-=nums[i]
+            s+=nums[j]
+            i+=1
+            j+=1
+            av=s/k
+            mx=max(mx,av)
+            
+        av=sum(nums[i:j])/k
+        mx=max(mx,av)
+        return(mx)
+            
