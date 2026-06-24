b1ca0ce53242cc4632ec466d0647aed5503ecbfc
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-06-24 15:58:42 +0530

    Time: 1577 ms (5.04%) | Memory: 22.4 MB (13.98%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.py
@@ -1,36 +1,24 @@
-import math
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        n=len(nums)
-        if k==0:
-            return 0
-        if math.prod(nums) < k:
-            return (n*(n+1)//2)
+        cnt=len([i for i in nums if i < k])
+        if prod(nums)<k:
+            return (len(nums) * (len(nums)+1))//2
         i=0
-        j=i
-        cnt=0
-        p=math.prod(nums[i:j+1])
-        while i < n:            
-            #print(i,j,p,cnt)
-            if  p< k:
+        j=1
+        d={}
+        while i<j and j<len(nums): 
+            p=prod(nums[i:j+1])           
+            print(j,d,cnt,p)
+            if p<k :
                 cnt+=1
-                j+=1
-                if j < n:
-                    p*=nums[j]
+                j+=1                
             else:
-                p//=nums[i]
+                p=p//nums[i]
                 i+=1
-                temp=math.prod(nums[i+1:j+1])
-                p//=temp
-                j=i
-                if j < n:
-                    p=nums[i]
-                #print(i,j,p,cnt)
-                
-            if i<n and j >=n:
-                p/=nums[i]
+                j=i+1
+            if j > len(nums)-1:
+                p=p//nums[i]                
                 i+=1
-                temp=math.prod(nums[i+1:j+1])
-                p//=temp     
-                j=i
+                j=i+1 
+        #print(cnt)
         return(cnt)
