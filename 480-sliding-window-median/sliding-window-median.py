92c8043c61ed51dced2deafeb6aabbb26616ce5d
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-06-30 14:39:03 +0530

    Time: 235 ms (30.72%) | Memory: 30.7 MB (86.58%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.py
+++ 480-sliding-window-median/sliding-window-median.py
@@ -3,21 +3,25 @@ class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
         i=0
         j=i+k
-        mid=k//2
-        l1=[]
-        flag=(1 if k%2 !=0 else 0)       
+        flag=1 if k%2!=0 else 0
         l=SortedList(nums[i:j])
-        
-        for i in range(k, len(nums)):                       
-            #print(i,mid,l)
+        l1=[]
+        while i < j and j < len(nums):
             if flag==1:
+                mid=k//2
                 l1.append(l[mid])
             else:
+                mid=k//2
                 l1.append((l[mid]+l[mid-1])/2)
-            l.remove(nums[i-k])
-            l.add(nums[i])
+            l.remove(nums[i])
+            i+=1
+            l.add(nums[j])
+            j+=1
         if flag==1:
+            mid=k//2
             l1.append(l[mid])
         else:
+            mid=k//2
             l1.append((l[mid]+l[mid-1])/2)
+        print(l1)
         return(l1)
