3ad59ae93ad8c6b38b75a76710e1ff62c43655e0
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-07-03 15:14:15 +0530

    Time: 935 ms (5.00%) | Memory: 20 MB (11.93%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -3,66 +3,32 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(t) > len(s):
             return ""
-        
-        s1=""
+        l=[]
         d=Counter(t)
-        
-
-        d1={}
+        t1={}
         i=0
         j=0
-        while i <= j and j < len(s):
-            flag=False
-            #print(i,j,d1)
+        mn=sys.maxsize
+        x=""
+        while i <=j and j < len(s):
+            word=s[i:j+1]
             if s[j] in d.keys():
-                if s[j] in d1.keys():
-                    d1[s[j]]+=1
+                if s[j] not in t1.keys():
+                    t1[s[j]]=1
                 else:
-                    d1[s[j]]=1
-            j+=1    
-            
-            
-            #print(d,d1,i,j,flag)
-            if len(d)==len(d1):
-                for k,v in d.items():
-                    if k not in d1.keys():
-                        flag=False
-                        break
-                    if v <= d1[k]:             
-                        flag=True
-                    else:
-                        flag=False
-                        break
-            while flag:   
-                #print(flag,i,j,d,d1)  
-                #print(s1,i,j,s[i:j],min(s1,s[i:j],key=len))
-                if s1=="":
-                    s1=s[i:j]
-                else:
-                    s1=min(s1,(s[i:j]),key=len)
-                
-                if s[i] in d1.keys():
-                    d1[s[i]]-=1
-                    if d1[s[i]]==0:
-                        del d1[s[i]]
-                i+=1
-                #print(d,d1,i,j,d.keys()==d1.keys(),l)
-                if len(d)==len(d1):               
-                    for k,v in d.items():
-                        #print(k,v,d1)
-                        if k not in d1.keys():
-                            flag=False
-                            break
-                        if v <= d1[k]:             
-                            flag=True
-                        else:
-                            flag=False
-                            break
-                else:
-                    flag=False
-                if not flag:
-                    #print("flag false")
-                    break
-                    
-            
-        return(s1)
+                    t1[s[j]]+=1
+            flag1=d.keys() == t1.keys() and all(t1[k] >= d[k] for k in d)
+            while flag1:
+                if j-i+1 < mn:
+                    mn=j-i+1
+                    x=s[i:j+1]
+                if s[i] in t1.keys():
+                    t1[s[i]]-=1
+                    if t1[s[i]]==0:
+                        del t1[s[i]]
+                i+=1  
+                flag1=d.keys() == t1.keys() and all(t1[k] >= d[k] for k in d)                
+            if not flag1:
+                j+=1
+        
+        return(x)
