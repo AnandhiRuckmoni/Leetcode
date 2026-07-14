d82774a3fe82a18a1740a112145c70aba54e366c
AnandhiRuckmoni <babasai.sr@gmail.com>
2026-07-14 16:36:30 +0530

    Time: 9 ms (5.97%) | Memory: 27.3 MB (99.77%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -3,76 +3,46 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
-    def __init__(self):
-        self.head=None
-        self.tail=None
-
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        tail=head
-        if head is None:
-            return head
-        if head.next is None:
-            return head
-        while tail.next:
-            tail=tail.next
-        if head.next is tail:
-            head.next=None
-            tail.next=head
-            head=tail
-            return head
-        else:
-            while head!=tail:
-                if tail.next:
-                    t=head
-                    head=head.next
-                    t.next=tail.next
-                    tail.next=t
-                else:
-                    t=head
-                    tail.next=head
-                    head=head.next
-                    t.next=None    
-        return head    
-
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        if head.next is None:
-            return head
         if head is None:
             return head
         curr=head
+        prev=None
+        succ=curr.next
+        while curr!=None:
+            curr.next=prev
+            prev=curr
+            curr=succ
+            if curr!=None:
+                succ=curr.next
+        head=prev
+        return head
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow=head
+        fast=head
+        l=[]
+        l.append(slow)
+        while fast!=None and fast.next!=None:            
+            slow=slow.next
+            fast=fast.next.next
+            l.append(slow)
+        #2nd list starts from slow.next and ends till null
+        ll2=self.reverseList(slow.next)
+        slow.next=None
+        
+        head2=ll2
         head1=head
-        nextp=curr
-        while nextp: 
-            prev=curr           
-            curr=curr.next
-            if nextp.next:
-                nextp=nextp.next.next
-            else:
-                break
-        prev.next=None
-        head2=curr
-        t=head
-        h3=self.reverseList(curr)
-        t1=h3
-        print(h3 is None)
-        while t and t1:
-            print(t.val,t1.val)
-            if t.next:
-                if t1 and t1.next:
-                    x=t1.next
-                else:
-                    x=None
-                t1.next=t.next
-                t.next=t1
-                t=t.next.next
-                t1=x
-            else:
-                t.next=t1
-                t=None
-                t1=None
-
-
-        
-        
+        #print(head2.val,head1.val)
+        while head2!=None:
+            print(head2.val)
+            t=head1.next
+            t2=head2.next
+            head1.next=head2
+            head2.next=t
+            head2=t2
+            head1=t
+            
